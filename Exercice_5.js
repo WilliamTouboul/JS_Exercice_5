b304cf5a4ea1255a6a580ba453ee1a32681e9458
William Touboul <Touboul.william@gmail.com>
2020-12-24 00:38:23 +0100

    Methode avec ParseInt et ParseFloat

--- Exercice_5.js
+++ Exercice_5.js
@@ -1,19 +1,8 @@
-function math() {
-    var regex = new RegExp("^[0-9\,\. ]+$");
-    let firstNumber = document.getElementById("firstNumber").value;
-    let secondNumber = document.getElementById("secondNumber").value;
-  
-    if (!regex.test(firstNumber) || firstNumber == null || firstNumber == '') {
-        alert('PAS BON');
-    }
-    else {
-        if(!regex.test(secondNumber) || firstNumber == null || firstNumber == '') {
-            alert('PAS BON 2');
-        }
-        else {
-            let firstNumberTrunc = Math.trunc(firstNumber);
-            let result = firstNumberTrunc * secondNumber;
-            alert('Le resultat est de : ' + result);
-        }
-    }
+function multiply() {
+    var firstNumber = parseInt((document.getElementById("firstNumber").value),10);
+    var secondNumber = parseFloat(document.getElementById("secondNumber").value);
+    var result = firstNumber * secondNumber;
+    alert ('Le resultat de votre multiplication est de' + result);
+
+    
 }
